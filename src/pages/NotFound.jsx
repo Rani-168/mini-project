7edceb92f94430c9286e7fe36eabbca25b86cc5f
Rani <ranibhosale168@gmail.com>
2026-04-02 21:31:58 +0530

    added react  page code

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,16 +1,16 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
-function NotFound() {
-  return (
-    <div style={{ textAlign: "center" }}>
-      <h1>❌ 404 - Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
+// function NotFound() {
+//   return (
+//     <div style={{ textAlign: "center" }}>
+//       <h1>❌ 404 - Page Not Found</h1>
+//       <p>The page you are looking for does not exist.</p>
 
-      <Link to="/" style={{ color: "blue", textDecoration: "underline" }}>
-        🔙 Go Back to Home
-      </Link>
-    </div>
-  );
-}
+//       <Link to="/" style={{ color: "blue", textDecoration: "underline" }}>
+//         🔙 Go Back to Home
+//       </Link>
+//     </div>
+//   );
+// }
 
-export default NotFound;
+// export default NotFound;
